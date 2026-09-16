9af1569c9b7c9cef5e2ecb1c8051873c56681dcf
benjaminrosso784-afk <benjaminrosso784@gmail.com>
2026-09-16 05:30:23 +0000

    clase16

---
+++ clase 16.py/clase16.py
@@ -0,0 +1,38 @@
+import requests
+import csv
+import os
+
+os.makedirs("csv", exist_ok=True)
+
+pokemenoes = []
+for i in range(1, 20):
+    url = f"https://pokeapi.co/api/v2/pokemon/{i}"
+    respuesta = requests.get(url)
+    data = respuesta.json()
+    p = {   
+        "id": data["id"],
+        "nombre": data["name"],
+        "peso": data["height"],
+        "altura": data["weight"],
+        "tipo": [t["type"]["name"] for t in data["types"]],
+        }
+    pokemenoes.append(p)
+
+with open("pokemons.csv", "w", newline="") as csvfile:
+    fieldnames = ["id", "nombre", "peso", "altura", "tipo"] 
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for p in pokemenoes:
+        writer.writerow(p)
+
+import sqlite3
+con = sqlite3.connect('pokemons.db')
+cur = con.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS pokemones"
+            "(nombre TEXT, peso INTEGER)")
+for s in pokemenoes:
+    cur.execute("INSERT INTO pokemones VALUES (? , ?)", (s["nombre"], s["peso"]))
+con.commit()
+for fila in cur.execute("SELECT * FROM pokemones"):
+    print(fila)
